41f0fd7864698e0302b2039e03ad0d7ef29ae539
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-11-27 14:28:20 -0300

    add config of cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express()
 const doctors = require("./routes/doctors")
-const cors = require("cors")
 
 app.use(express.json())
 
@@ -13,7 +12,16 @@ app.use((req, res, next)=>{
     next();
 });
 
-app.use(cors())
+app.options("/*", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers")
+    res.header(
+        "Access-Control-Allow-Methods",
+        "PUT,POST,GET,DELETE,OPTIONS,PATCH"
+    );
+    res.send("send some thing whatever")
+})
+
 app.use("/", index)
 app.use("/doctors", doctors)
 
